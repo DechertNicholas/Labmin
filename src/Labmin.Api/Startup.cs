04655d57cdfa1b1078c9f44eec0c9cedcdb456ce
Nicholas Dechert <dechert.nicholas@gmail.com>
2020-12-23 00:14:53 -0800

    API now accessible with error, as database is not yet configured

--- src/Labmin.Api/Startup.cs
+++ src/Labmin.Api/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -30,7 +31,10 @@ namespace Labmin.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<LabminDbContext>();
+            //services.AddScoped(typeof(DbContext), typeof(LabminDbContext));
+            
+            services.AddDbContext<LabminDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("LabminDBContext")));
+            services.AddScoped<DbContext, LabminDbContext>();
             services.AddScoped<IRepository<Pool>, EfCoreSqlPoolRepository>();
             services.AddScoped<IPoolService, PoolService>();
             services.AddControllers();
