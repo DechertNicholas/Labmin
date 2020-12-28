a21274c6e84cffac0c06e333d195fafb4736ed89
Nicholas Dechert <dechert.nicholas@gmail.com>
2020-12-27 22:55:41 -0800

    Controller get all pool and get single pool by name is working Commenting out Put, Post, Delete for now - will be tied to authorization later but not currently necessary

--- src/Labmin.Api/Controllers/PoolsController.cs
+++ src/Labmin.Api/Controllers/PoolsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Labmin.Api.Data;
 using Labmin.Core.Models;
+using Labmin.Api.Services;
 
 namespace Labmin.Api.Controllers
 {
@@ -14,95 +15,90 @@ namespace Labmin.Api.Controllers
     [ApiController]
     public class PoolsController : ControllerBase
     {
-        private readonly LabminDbContext _context;
+        private readonly IPoolService _poolService;
 
-        public PoolsController(LabminDbContext context)
+        public PoolsController(IPoolService poolService)
         {
-            _context = context;
+            _poolService = poolService;
         }
 
-        // GET: api/Pools
+        // GET: api/v1/Pools
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Pool>>> GetPools()
+        public async Task<IEnumerable<Pool>> GetPools()
         {
-            return await _context.Pools.ToListAsync();
+            return await _poolService.ReadAllAsync();
         }
 
-        // GET: api/Pools/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Pool>> GetPool(int id)
+        // GET: api/v1/Pools/5
+        [HttpGet("{name}")]
+        public async Task<ActionResult<Pool>> GetPool(string name)
         {
-            var pool = await _context.Pools.FindAsync(id);
-
-            if (pool == null)
-            {
-                return NotFound();
-            }
+            var pool = await _poolService.ReadOneAsync(name);
 
             return pool;
         }
 
-        // PUT: api/Pools/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutPool(int id, Pool pool)
-        {
-            if (id != pool.Id)
-            {
-                return BadRequest();
-            }
+        //// PUT: api/v1/Pools/5
+        //// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        //[HttpPut("{id}")]
+        //public async Task<IActionResult> PutPool(int id, Pool pool)
+        //{
+        //    if (id != pool.Id)
+        //    {
+        //        return BadRequest();
+        //    }
 
-            _context.Entry(pool).State = EntityState.Modified;
+        //    _poolService.Entry(pool).State = EntityState.Modified;
 
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!PoolExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+        //    try
+        //    {
+        //        await _poolService.SaveChangesAsync();
+        //    }
+        //    catch (DbUpdateConcurrencyException)
+        //    {
+        //        if (!PoolExists(id))
+        //        {
+        //            return NotFound();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
 
-            return NoContent();
-        }
+        //    return NoContent();
+        //}
 
-        // POST: api/Pools
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<Pool>> PostPool(Pool pool)
-        {
-            _context.Pools.Add(pool);
-            await _context.SaveChangesAsync();
+        //// POST: api/v1/Pools
+        //// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        //[HttpPost]
+        //public async Task<ActionResult<Pool>> PostPool(Pool pool)
+        //{
+        //    _poolService.Pools.Add(pool);
+        //    await _poolService.SaveChangesAsync();
 
-            return CreatedAtAction("GetPool", new { id = pool.Id }, pool);
-        }
+        //    return CreatedAtAction("GetPool", new { id = pool.Id }, pool);
+        //}
 
-        // DELETE: api/Pools/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeletePool(int id)
-        {
-            var pool = await _context.Pools.FindAsync(id);
-            if (pool == null)
-            {
-                return NotFound();
-            }
+        //// DELETE: api/v1/Pools/5
+        //[HttpDelete("{id}")]
+        //public async Task<IActionResult> DeletePool(int id)
+        //{
+        //    var pool = await _poolService.Pools.FindAsync(id);
+        //    if (pool == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            _context.Pools.Remove(pool);
-            await _context.SaveChangesAsync();
+        //    _poolService.Pools.Remove(pool);
+        //    await _poolService.SaveChangesAsync();
 
-            return NoContent();
-        }
+        //    return NoContent();
+        //}
 
-        private bool PoolExists(int id)
-        {
-            return _context.Pools.Any(e => e.Id == id);
-        }
+        //private bool PoolExists(int id)
+        //{
+        //    return _poolService.Pools.Any(e => e.Id == id);
+        //}
     }
 }
