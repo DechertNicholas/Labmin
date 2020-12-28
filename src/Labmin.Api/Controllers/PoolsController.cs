3451258069c71964538d4cd33a7b69a4660ac6df
Nicholas Dechert <dechert.nicholas@gmail.com>
2020-12-27 18:38:24 -0800

    Update to .NET 5.0 Update to .Net Standard 2.1 Scaffold whole controller for Pools Add PoolsControllerTest file Create initial DB migration for Pool model

--- src/Labmin.Api/Controllers/PoolsController.cs
+++ src/Labmin.Api/Controllers/PoolsController.cs
@@ -1,29 +1,108 @@
-﻿using Labmin.Api.Services;
-using Labmin.Core.Models;
-using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Labmin.Api.Data;
+using Labmin.Core.Models;
 
 namespace Labmin.Api.Controllers
 {
-    [Route("v1/[controller]")]
-    public class PoolsController : Controller
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class PoolsController : ControllerBase
     {
-        private readonly IPoolService _poolService;
+        private readonly LabminDbContext _context;
 
-        public PoolsController(IPoolService poolService)
+        public PoolsController(LabminDbContext context)
         {
-            _poolService = poolService ?? throw new ArgumentNullException(nameof(poolService));
+            _context = context;
         }
 
+        // GET: api/Pools1
         [HttpGet]
-        [ProducesResponseType(typeof(IEnumerable<Pool>), 200)]
-        public async Task<IActionResult> ReadAllAsync()
+        public async Task<ActionResult<IEnumerable<Pool>>> GetPools()
         {
-            var allPools = await _poolService.ReadAllAsync();
-            return Ok(allPools);
+            return await _context.Pools.ToListAsync();
+        }
+
+        // GET: api/Pools1/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Pool>> GetPool(int id)
+        {
+            var pool = await _context.Pools.FindAsync(id);
+
+            if (pool == null)
+            {
+                return NotFound();
+            }
+
+            return pool;
+        }
+
+        // PUT: api/Pools1/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPool(int id, Pool pool)
+        {
+            if (id != pool.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(pool).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PoolExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Pools1
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Pool>> PostPool(Pool pool)
+        {
+            _context.Pools.Add(pool);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPool", new { id = pool.Id }, pool);
+        }
+
+        // DELETE: api/Pools1/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePool(int id)
+        {
+            var pool = await _context.Pools.FindAsync(id);
+            if (pool == null)
+            {
+                return NotFound();
+            }
+
+            _context.Pools.Remove(pool);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PoolExists(int id)
+        {
+            return _context.Pools.Any(e => e.Id == id);
         }
     }
 }
---
+++ test/Labmin.ApiUnitTests/Controllers/PoolsControllerTest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labmin.ApiUnitTests.Controllers
+{
+    class PoolsControllerTest
+    {
+    }
+}
